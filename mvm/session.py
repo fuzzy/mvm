9181e351f605bb71dfcad0746b32647825da912d
Mike 'Fuzzy' Partin <fuzzy@fumanchu.org>
2016-04-01 07:01:11 -0700

    added session support

---
+++ mvm/session.py
@@ -0,0 +1,20 @@
+# Stdlib imports
+import os
+import json
+import time
+
+# Internal imports
+from mvm.term import *
+
+class MvmSession(object):
+
+    def _startSession(self):
+        dataFile                 = self.config.dirs.profiles+'/session.json'
+        data                     = json.loads(open(dataFile).read())
+        if self.SessionID not in data.keys():
+            data[self.SessionID] = {"timestamp": time.time()}
+            open(dataFile, 'w+').write(json.dumps(data))
+        sessionDir               = '%s/%s' % (self.config.dirs.sessionroot,
+                                              self.SessionID)
+        if not os.path.isdir(sessionDir):
+            os.mkdir(sessionDir)
