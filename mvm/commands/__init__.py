540e5c007a28331ce1efe1ab34e7ad74593d74ec
Mike 'Fuzzy' Partin <fuzzy@fumanchu.org>
2016-04-04 10:44:08 -0700

    added stub for remove command

--- mvm/commands/__init__.py
+++ mvm/commands/__init__.py
@@ -1,6 +1,7 @@
 from mvm.commands.pkglist import *
 from mvm.commands.enable  import *
 from mvm.commands.install import *
+from mvm.commands.remove  import *
 
-class MvmCommands(MvmCmdList, MvmCmdEnable, MvmCmdInstall):
+class MvmCommands(MvmCmdList, MvmCmdEnable, MvmCmdInstall, MvmCmdRemove):
     pass
