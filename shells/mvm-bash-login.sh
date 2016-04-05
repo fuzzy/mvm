b283395a6e72c3eb21420f3d171b6c978e9d840d
Mike 'Fuzzy' Partin <fuzzy@fumanchu.org>
2016-04-05 16:45:14 -0700

    fixed snafu in bash-login.sh

--- shells/mvm-bash-login.sh
+++ shells/mvm-bash-login.sh
@@ -3,8 +3,8 @@ export MVM_SESSION_ID=$(mvm-session)
 export MVM_GLOBAL_DIR=${HOME}/.mvm/sessions/global
 export MVM_SESSION_DIR=${HOME}/.mvm/sessions/local/${MVM_SESSION_ID}
 
-test ! -d ${MVM_GLOBAL_DIR} && mkdir ${MVM_GLOBAL_DIR}
-test ! -d ${MVM_SESSION_DIR} && mkdir ${MVM_SESSION_DIR}
+test ! -d ${MVM_GLOBAL_DIR} && mkdir -p ${MVM_GLOBAL_DIR}
+test ! -d ${MVM_SESSION_DIR} && mkdir -p ${MVM_SESSION_DIR}
 
 export PATH=${MVM_GLOBAL_DIR}/bin:${MVM_GLOBAL_DIR}/sbin:${MVM_SESSION_DIR}/bin:${MVM_SESSION_DIR}/sbin:${PATH}
 
