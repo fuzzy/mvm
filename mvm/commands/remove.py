47bd1f4ff343870dda2dcb5cb4a23823e40f4b85
Mike 'Fuzzy' Partin <fuzzy@fumanchu.org>
2016-04-04 23:12:42 -0700

    Finished off RemovePackage method of Mvm(), adding a Delete method to the Package() class next.

--- mvm/commands/remove.py
+++ mvm/commands/remove.py
@@ -1,15 +1,38 @@
 
 # Stdlib imports
+import os
+import sys
 
 # Internal imports
-from mvm.term import *
+from mvm.term    import *
+from mvm.package import *
 
 class MvmCmdRemove(object):
 
     def RemovePackage(self, arg=None, session=True):
-        debug('arg=%r session=%r' % (arg, session))
         if session:
             cdir = self.config.dirs.sessionroot+'/'+self.SessionID
         else:
             cdir = self.config.dirs.globalroot
 
+        pkg      = arg.split('-')[0]
+        vers     = '-'.join(arg.split('-')[1:])
+        target   = self.config.dirs.instroot
+        obj      = False
+
+        for i in os.listdir(target):
+            if i.lower() == pkg.lower():
+                target += '/'+pkg
+                if os.path.isdir(target+'/'+self.sysInfo.os.name.lower()):
+                    target += '/'+self.sysInfo.os.name.lower()
+                    if os.path.isdir(target+'/'+self.sysInfo.os.arch):
+                        target += '/'+self.sysInfo.os.arch
+                        if os.path.isdir(target+'/'+vers):
+                            target += '/'+vers
+                            obj     = Package(self.config, target)
+
+        if not obj:
+            fatal('The package you specified is not installed.')
+            sys.exit(1)
+        else:
+            obj.Delete()
