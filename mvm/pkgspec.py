5caf3ec4718c6a597fef5aa7de04d52da93bb3da
Mike 'Fuzzy' Partin <fuzzy@fumanchu.org>
2016-04-09 11:15:10 -0700

    more rewrite in mvm/pkgspec.py

--- mvm/pkgspec.py
+++ mvm/pkgspec.py
@@ -33,20 +33,20 @@ class PkgSpec(object):
     ##
     ## Class attributes
 
-    system             = Edict({'osname': os.uname()[0],
-                               'osvers': os.uname()[2],
-                               'osarch': os.uname()[4],
-                               'cores':  multiprocessing.cpu_count()})
-
-    defaults           = Edict({'packager':  None,  'email':   None,
-                                'homepage':  None,  'package': None,
-                                'version':   None,  'source':  None,
-                                'license':   None,
+    _defaults          = Edict({'osname':    os.uname()[0].lower(),
+                                'osvers':    os.uname()[2].lower(),
+                                'osarch':    os.uname()[4].lower(),
+                                'cores':     multiprocessing.cpu_count(),
+                                'prefix':    'X',           'packager':  None,
+                                'email':     None,          'homepage':  None,
+                                'package':   None,          'version':   None,
+                                'source':    None,          'license':   None,
+                                'patches':   [],            'depends':   [],
                                 'configure': {'cmd':  './configure',
-                                               'args': ['--prefix=%%PREFIX%%',],
+                                               'args': ['--prefix=%prefix%',],
                                                'env':  []},
                                 'compile':   {'cmd':  'make',
-                                               'args': ['-j%%CORES%%',],
+                                               'args': ['-j%cores%',],
                                                'env':  []},
                                 'install':   {'cmd':  'make',
                                                'args': ['install',],
@@ -70,42 +70,123 @@ class PkgSpec(object):
         if False in (pSpec, config):
             raise(ValueError, 'Must supply a package spec, and a config object.')
         # First let's record our config object
-        self._cfg      = config
+        self._cfg            = config
         # And then validate we have a real file
         if os.path.isfile(pSpec):
-            self._spec = pSpec
+            self._spec       = pSpec
         elif os.path.isfile(self._cfg.dirs.pkgspecs+'/'+pSpec):
-            self._spec = self._cfg.dirs.pkgspecs+'/'+pSpec
-        # Now let's build our map of defaults
-        self.defaults  = Edict({'configure': {'cmd': './configure',
-                                              'args': ['--prefix=%s/%s/%s/%s/%s' % (self._cfg.dirs.pkgspecs, '', '', '', ''),]},
-                                'compile':   {'cmd': 'make',
-                                              'args': ['-j%%CORES%%' % ]},
-                                'install':   {}})
+            self._spec       = self._cfg.dirs.pkgspecs+'/'+pSpec
+        # Let's read in our JSON data
+        try:
+            self._data       = Edict(json.loads(open(self._spec).read()))
+        except Exception:
+            fatal("The file %s contains malformed JSON data." % self._spec)
+            sys.exit(1)
+        # Now let's update our defaults map
+        self._defaults.prefix = '%s/%s/%s/%s/%s' % (self._cfg.dirs.instroot,
+                                                    self._data.package.lower(),
+                                                    self._defaults.osname,
+                                                    self._defaults.osarch,
+                                                    self._data.version.lower())
+        print(self._cfg.dirs)
+        self._lint()
+        print('self._data: %s' % self._data)
+        self.data             = self._macros(self._data.copy())
+        print('self._data: %s' % self._data)
 
     def __str__(self):
         return str('pkgspec')
 
     def __repr__(self):
-        print str(self.__str__())
+        print(str(self.__str__()))
 
     ##
     ## Private Methods
 
-    def _fetch(self):
-        pass
+    def _lint(self):
+        for k in self._defaults.keys():
+            if k not in self._data.keys():
+                if self._defaults[k] != None:
+                    self._data[k] = self._defaults[k]
+                else:
+                    fatal("Required keyword %s is missing from %s." % (k, self._spec))
+                    sys.exit(1)
+
+    def _macros(self, data=False):
+        if not data:
+            raise(ValueError, 'No data structure supplied to PackageSpec._expandMacros()')
+        for itm in data.keys():
+            if isinstance(data[itm], dict):
+                tdata = self._macros(data[itm])
+                data[itm] = tdata
+            elif isinstance(data[itm], (list, tuple)):
+                for initm in range(0, len(data[itm])):
+                    if isinstance(data[itm][initm], str_types):
+                        tmps = data[itm][initm]
+                        tmpk = ''
+                        for macro in re.findall(r'%[a-zA-Z0-9_]*%', data[itm][initm]):
+                            tmpk = re.sub('%', '', macro)
+                            tmps = re.sub(macro, str(self._data[tmpk]), data[itm][initm])
+                            data[itm][initm] = tmps
+            elif isinstance(data[itm], str_types):
+                tmps = data[itm]
+                tmpk = ''
+                for macro in re.findall(r'%[a-zA-Z0-9_]*%', data[itm]):
+                    tmpk = re.sub('%', '', macro)
+                    tmps = re.sub(macro, self._data[tmpk], tmps)
+                data[itm] = tmps
+        return data
+
+    def _fetch(self, uri):
+        output    = '%s/%s' % (self._cfg.dirs.dstfiles,
+                               os.path.basename(uri))
+        print('%s%s %s' % (cyan('>'), white('>'), uri))
+        inFP      = urlopen(uri)
+        outFP     = open(output, 'wb+')
+        buffsize  = (1024 * 40)
+        buff      = inFP.read(buffsize)
+        outsz     = 0
+        st        = time.time()
+        while buff:
+            outFP.write(buff)
+            outFP.flush()
+            buff  = inFP.read(buffsize)
+            outsz += len(buff)
+            tlen  = int(time.time() - st)
+            if tlen == 0: tlen = 1
+            sys.stdout.write('%s%s %s in %s @ %s/sec%s\r' % (cyan('>'),
+                                                             white('>'),
+                                                             humanSize(outsz),
+                                                             humanTime(tlen),
+                                                             humanSize(outsz / tlen),
+                                                             ' '*10))
+            sys.stdout.flush()
+        print('')
 
     def _extract(self):
         pass
 
-    def _execCmd(self):
-        pass
+    def _cmd(self, dname, data):
+        cdir = os.getcwd()
+        env  = []
 
-    def _lint(self):
-        pass
+        os.chdir(dname)
+        for e in data['env']:
+            env.append('='.join(e))
 
-    def _expandMacros(self):
-        pass
+        cmd  = 'env %s %s %s' % (' '.join(env), data['cmd'], ' '.join(data['args']))
+        proc = subprocess.Popen(cmd,
+                                shell=True,
+                                stdin=subprocess.PIPE,
+                                stdout=subprocess.PIPE,
+                                stderr=subprocess.STDOUT,
+                                close_fds=True)
+        logfile = '%s/%s-%s.log' % (self._cfg.dirs.pkgtemp,
+                                    self._data['package'].lower(),
+                                    self._data['version'])
+        open(logfile, 'ab').write(proc.stdout.read())
+        #os.system('env %s %s %s' % (' '.join(env), data['cmd'], ' '.join(data['args'])))
+        os.chdir(cdir)
 
     ##
     ## Public Methods
@@ -127,145 +208,11 @@ class PkgSpec(object):
 
 class PackageSpec(object):
 
-    osname       = os.uname()[0]
-    arch         = os.uname()[-1:][0]
-    cores        = multiprocessing.cpu_count()
-    prefix_b     = '/.mvm/packages/instroot/%package%/%osnamel%/%arch%/%version%'
-    keywords     = {'required': ('packager',  'email',   'homepage',
-                                 'package',   'version', 'source',
-                                 'license'),
-                    'optional': ('patches',),
-                    'methods':  ('configure', 'compile', 'install')}
-
-    # Class Methods
-    def __init__(self, pkgSpec=False):
-        if not pkgSpec or not os.path.isfile(pkgSpec):
-            raise(ValueError, 'Absent package spec.')
-        self.defaults       = {'configure': {'cmd':    './configure',
-                                             'args':   ['--prefix=%s%s' % (os.getenv('HOME'),
-                                                                           self.prefix_b),],
-                                             'env':    [],
-                                             "enable": True},
-                               'compile':   {'cmd':    'make',
-                                             'args':   ['-j%d' % self.cores,],
-                                             'env':    [],
-                                             "enable": True},
-                               'install':   {'cmd':    'make',
-                                             'args':   ['install',],
-                                             'env':    [],
-                                             "enable": True},
-                               'osname':    self.osname,
-                               'osnamel':   self.osname.lower(),
-                               'arch':      self.arch}
-        try:
-            data            = json.loads(open(pkgSpec).read())
-        except Exception:
-            fatal('%s is not valid json.' % pkgSpec)
-            raise
-        data['prefix']      = os.getenv('HOME')+'/'+self.prefix_b
-        self.data           = self._lint(data)
-        self.data           = self._expandMacros(self.data)
-        tdata               = self.data['prefix'].lower()
-        self.data['prefix'] = tdata
-        debug(tdata)
-
-    ##
-    ## Private methods
-    def _lint(self, data=False):
-        if not data:
-            raise(ValueError, 'No data structure supplied to PackageSpec._lint().')
-        for kwt in self.keywords.keys():
-            for kw in self.keywords[kwt]:
-                if kw not in data.keys() and kwt == 'required':
-                    fatal('required keyword %s is not in the pkgSpec.' % kw)
-                    sys.exit(1)
-        for dflt in self.defaults.keys():
-            if dflt in data.keys():
-                for attr in self.defaults[dflt].keys():
-                    if attr not in data[dflt].keys():
-                        data[dflt][attr] = self.defaults[dflt][attr]
-            else:
-                data[dflt] = self.defaults[dflt]
-        return data # self._expandMacros(data)
-
-    def _expandMacros(self, data=False):
-        if not data:
-            raise(ValueError, 'No data structure supplied to PackageSpec._expandMacros()')
-        for itm in data.keys():
-            if isinstance(data[itm], dict):
-                tdata = self._expandMacros(data[itm])
-                data[itm] = tdata
-            elif isinstance(data[itm], (list, tuple)):
-                for initm in range(0, len(data[itm])):
-                    if isinstance(data[itm][initm], str_types):
-                        tmps = data[itm][initm]
-                        tmpk = ''
-                        for macro in re.findall(r'%[a-zA-Z0-9_]*%', data[itm][initm]):
-                            tmpk = re.sub('%', '', macro)
-                            tmps = re.sub(macro, self.data[tmpk], data[itm][initm])
-                            data[itm][initm] = tmps
-            elif isinstance(data[itm], str_types):
-                tmps = data[itm]
-                tmpk = ''
-                for macro in re.findall(r'%[a-zA-Z0-9_]*%', data[itm]):
-                    tmpk = re.sub('%', '', macro)
-                    tmps = re.sub(macro, self.data[tmpk], tmps)
-                data[itm] = tmps
-        return data
-
-    def _fetch(self, uri):
-        output    = '%s/.mvm/packages/distfiles/%s' % (os.getenv('HOME'),
-                                                    os.path.basename(uri))
-        print('%s%s %s' % (cyan('>'), white('>'), uri))
-        inFP      = urlopen(uri)
-        outFP     = open(output, 'wb+') # .write(urlopen(uri).read())
-        buffsize  = 40960
-        buff      = inFP.read(buffsize)
-        outsz     = 0
-        st        = time.time()
-        while buff:
-            outFP.write(buff)
-            outFP.flush()
-            buff  = inFP.read(buffsize)
-            outsz += len(buff)
-            tlen  = int(time.time() - st)
-            if tlen == 0: tlen = 1
-            sys.stdout.write('%s%s %s in %s @ %s/sec%s\r' % (cyan('>'),
-                                                             white('>'),
-                                                             humanSize(outsz),
-                                                             humanTime(tlen),
-                                                             humanSize(outsz / tlen),
-                                                             ' '*10))
-            sys.stdout.flush()
-        print('')
-
     def _extract(self, fname):
         distfiles = '%s/.mvm/packages/distfiles' % os.getenv('HOME')
         tempdir   = '%s/.mvm/packages/temp'      % os.getenv('HOME')
         print('%s%s Extracting %s' % (cyan('>'), white('>'), fname))
         os.system('bsdtar xpf %s/%s -C %s' % (distfiles, fname, tempdir))
-
-    def _cmd(self, dname, data):
-        cdir = os.getcwd()
-        env  = []
-
-        os.chdir(dname)
-        for e in data['env']:
-            env.append('='.join(e))
-
-        cmd  = 'env %s %s %s' % (' '.join(env), data['cmd'], ' '.join(data['args']))
-        proc = subprocess.Popen(cmd,
-                                shell=True,
-                                stdin=subprocess.PIPE,
-                                stdout=subprocess.PIPE,
-                                stderr=subprocess.STDOUT,
-                                close_fds=True)
-        logfile = '%s/.mvm/packages/temp/%s-%s.log' % (os.getenv('HOME'),
-                                                       self.data['package'].lower(),
-                                                       self.data['version'])
-        open(logfile, 'ab').write(proc.stdout.read())
-        #os.system('env %s %s %s' % (' '.join(env), data['cmd'], ' '.join(data['args'])))
-        os.chdir(cdir)
 
     def _configure(self):
         if self.data['configure']['enable']:
