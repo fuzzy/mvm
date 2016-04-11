1b8093a36c88e4dcabf05dd3ae7d2f0fe016dfa0
Mike 'Fuzzy' Partin <fuzzy@fumanchu.org>
2016-04-11 00:32:41 -0700

    initial work on dependancy tracking

--- mvm/commands/install.py
+++ mvm/commands/install.py
@@ -7,6 +7,7 @@
 import os
 
 # Internal imports
+from mvm.config  import *
 from mvm.pkgspec import *
 
 ###############################################################################
@@ -14,11 +15,18 @@ class MvmCmdInstall(object):
 
     def InstallPackage(self, force=False, clean=False, verbose=False, arg=False):
         if arg:
-            pkg = arg.split('-')[0]
-            ver = '-'.join(arg.split('-')[1:])
+            pkg   = arg.split('-')[0]
+            ver   = '-'.join(arg.split('-')[1:])
+            specs = Elist()
             if os.path.isfile('%s/%s/%s.json' % (self.config.dirs.pkgspecs, pkg, ver)):
-                spec = PackageSpec(pSpec='%s/%s/%s.json' % (self.config.dirs.pkgspecs, pkg, ver),
-                                   config=self.config)
+                specs.append(PackageSpec(pSpec='%s/%s/%s.json' % (self.config.dirs.pkgspecs, pkg, ver),
+                                         config=self.config))
+                # Now let's plow through our depends list, and build our whole install list
+                if len(specs[0].Depends()) > 0:
+                    for dep in specs[0].Depends():
+                        specs.append(PackageSpec(pSpec='%s.json' % dep, config=self.config))
+                        for dep in specs.last().Depends():
+
                 spec.Build(force=force, clean=clean, verbose=verbose)
         else:
             raise(ValueError, 'Specfile does not exist in %s' % self.config.dirs.pkgspecs)
